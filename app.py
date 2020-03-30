f5b5f79554e763e666108273b3928694dfd0b6e6
Nikita Baryshev <nbasharkin@gmail.com>
2020-03-30 17:12:09 +0300

    managing CORS

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/covidData/*": {"origins": "*"}})
 
 
 @app.route('/covidData', methods=('GET', 'POST'))
